103e976046545c68566b8962644189b6102ac9aa
2394974064qq <48664564+2394974064qq@users.noreply.github.com>
2020-11-30 11:40:03 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class App extends R {
+	public static void main(String[] args) {
+		App app=new App();
+		 System.out.println(app.boy_filePath);
+		 System.out.println(app.girl_filePath);		 
+		 
+		 for(int i=0;i<app.arr.length;i++) {
+			 for(int j=0;j<app.arr[i].length;j++) {
+				 System.out.println(app.arr[i][j]);
+			 }
+		}
+	}
+}
