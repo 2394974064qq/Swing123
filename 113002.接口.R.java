7c1e49181f738a3cee7f22ae02a0e4ccf0de7b9a
2394974064qq <48664564+2394974064qq@users.noreply.github.com>
2020-11-30 11:42:15 +0800

    Create 113002.接口.R.java

---
+++ 113002.接口.R.java
@@ -0,0 +1,10 @@
+package pack;
+
+public interface R {
+	//R类里面有字符串型变量：
+	String boy_filePath="img/boy.gif";
+	String girl_filePath="img/girl.gif";
+	int [][]arr={{1,2,3},{4,5,6}};
+	
+	//在App主类里面实现对R类的继承，并输出这些变量。
+}
